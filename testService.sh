98745f78722e61b3aff760914be047ece3cff613
121012648@qq.com <121012648@qq.com>
2017-07-13 19:19:01 +0800

    后期应该将clientId改为和topic类似的使用方法,payload也要支持用户自定义输入

--- testService.sh
+++ testService.sh
@@ -4,4 +4,4 @@
 payload={\"datas\":{\"green\":\"010402000178F0\",\"ngnum\":\"01040270295D2E\",\"oknum\":\"010402B914CAAF\",\"red\":\"0104020000B930\",\"total\":\"0104020000B930\",\"yellow\":\"0104020000B930\"},\"uuid\":\"uuid_1\"}
 
 
-./emqtt_bench_pub -c 10 -i 100 -I 1000 -t bench/%i -u janpos -P janpos -h 192.168.0.22 -L %i
+./emqtt_bench_pub -c 10 -i 100 -I 1000 -t bench/%i -u janpos -P janpos -h 192.168.0.22 

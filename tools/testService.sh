34ca50fc2ee67709051ca5aeb9ed2189f6b933a5
121012648@qq.com <121012648@qq.com>
2017-09-21 18:54:19 +0800

    最开始检测服务状态

--- tools/testService.sh
+++ tools/testService.sh
@@ -6,6 +6,14 @@ curFileRelCallPath=$(dirname "$0")
 cd $curFileRelCallPath
 
 # ----------------------
+#     SERVICE STATUS TEST
+# ----------------------
+sudo service odevice status
+sudo service oeboard status
+sudo service ocollector status
+sudo service oprotocol  status
+
+# ----------------------
 #     SAVE TEST
 # ----------------------
 echo ----------------------------------
@@ -18,17 +26,21 @@ echo "              MQTT PUB PARALLEL"
 echo ----------------------------------
 cd ../
 #../emqtt_bench_pub -c 100 -i 10 -I 1000 -t bench/%i -u janpos -P janpos -h 192.168.0.22
-./emqtt_bench_pub -c 50 -i 100 -I 1000 -t omma/device/data -u janpos -P janpos -h 192.168.0.30 --pubcount 100 --workmode send 
+./emqtt_bench_pub -c 50 -i 100 -I 1000 -t omma/device/data -u janpos -P janpos -h localhost --pubcount 1000 --workmode send 
 cd tools
 
 echo ----------------------------------
-echo "              DB CHECK"
+echo "              DB SAVE CHECK"
 echo ----------------------------------
-./exampleinfluxdbcheck check
+./exampleinfluxdbcheck save
 
 # ----------------------
 #     REQUEST TEST
 # ----------------------
+echo ----------------------------------
+echo "              DB QEURY RESPONSE CHECK"
+echo ----------------------------------
+./exampleinfluxdbcheck query
 
 
 # ../emqtt_bench_pub -c 50 -i 100 -I 1000 -t omma/influxClient/query -u janpos -P janpos -h 101.37.69.122 --workmode request
